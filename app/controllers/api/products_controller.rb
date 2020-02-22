99f0bdf9696c6a2b95d2ccf8ffb77a4fe53c095d
robocop019 <chrisbowen@comcast.net>
2020-02-22 17:01:34 -0600

    created route, controller action, and view to display one product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,17 @@
 class Api::ProductsController < ApplicationController
+
   def all_products_action
     @products = Product.all
     render 'all_products_view.json.jbuilder'
+  end
+
+  def first_product_action
+    @product = Product.find(1)
+    render 'first_product_view.json.jbuilder'
+  end
+
+  def second_product_action
+    @product = Product.find(2)
+    render 'second_product_view.json.jbuilder'
   end
 end
