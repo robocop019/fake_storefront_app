541573283fa653096415c458c4e1391d3cb301c6
robocop019 <chrisbowen@comcast.net>
2020-02-22 16:49:29 -0600

    created seeds file and added to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+product = Product.new(name: 'Jose Abreu Jersey', price: 250, description: 'Authenic replica jersey worn by number 79 of the Chicago White Sox.')
+product.save
+
+product = Product.new(name: 'Khalil Mack Jersey', price: 150, description: 'Replica jersey of Kahlil Mack from the Chicago Bears.')
+product.save
+
+product = Product.new(name: 'Signed Paul Konerko Bat', price: 500, description: 'Signed game used bat by Paul Konerko of the Chicago White Sox.')
+product.save
+
+product = Product.new(name: 'Signed Patrick Kane Jersey', price: 300, description: 'Game worn jersey signed by #88 Patrick Kane of the Chicago Blackhawks.')
+product.save
+
+product = Product.new(name: 'Cheese Grater Hat', price: 35, description: 'Foam cheese grater hat, perfect to wear when the Chicago Bears take on the green bay packers.')
+product.save
