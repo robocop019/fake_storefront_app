725a69b03df6645713c964579d39690a4c0940de
robocop019 <chrisbowen@comcast.net>
2020-02-24 22:39:50 -0600

    added create restful route

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,6 +5,17 @@ class Api::ProductsController < ApplicationController
     render 'index.json.jbuilder'
   end
 
+  def create
+    @product = Product.new(
+                          name: params[:name],
+                          price: params[:price],
+                          image_url: params[:image_url],
+                          description: params[:description]
+                          )
+    @product.save
+    render 'show.json.jbuilder'
+  end
+
   def show
     @product = Product.find(params[:id])
     render 'show.json.jbuilder'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do 
-    get '/all_products_url' => 'products#all_products_action'
-    get '/first_product_url' => 'products#first_product_action'
-    get '/second_product_url' => 'products#second_product_action'
-
     get '/products' => 'products#index'
+    post '/products' => 'products#create'
     get '/products/:id' => 'products#show'
   end
 end
