a7e21e16ef39ec1522f902753b64844e2878c0f8
robocop019 <chrisbowen@comcast.net>
2020-02-26 12:39:28 -0600

    added seed dump gem

---
+++ db/old_seeds.rb
@@ -0,0 +1,14 @@
+product = Product.new(name: 'Jose Abreu Jersey', price: 250, description: 'Authenic replica jersey worn by number 79 of the Chicago White Sox.')
+product.save
+
+product = Product.new(name: 'Khalil Mack Jersey', price: 150, description: 'Replica jersey of Kahlil Mack from the Chicago Bears.')
+product.save
+
+product = Product.new(name: 'Signed Paul Konerko Bat', price: 500, description: 'Signed game used bat by Paul Konerko of the Chicago White Sox.')
+product.save
+
+product = Product.new(name: 'Signed Patrick Kane Jersey', price: 300, description: 'Game worn jersey signed by #88 Patrick Kane of the Chicago Blackhawks.')
+product.save
+
+product = Product.new(name: 'Cheese Grater Hat', price: 35, description: 'Foam cheese grater hat, perfect to wear when the Chicago Bears take on the green bay packers.')
+product.save
