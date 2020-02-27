23ccee01af17f323462ca0927c4ef184b3ac959f
robocop019 <chrisbowen@comcast.net>
2020-02-27 16:20:50 -0600

    added routes for suppliers and images

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,18 @@ Rails.application.routes.draw do
     get '/products/:id' => 'products#show'
     patch '/products/:id' => 'products#update'
     delete '/products/:id' => 'products#destroy'
+
+    get '/suppliers' => 'suppliers#index'
+    post '/suppliers' => 'suppliers#create'
+    get '/suppliers/:id' => 'suppliers#show'
+    patch '/suppliers/:id' => 'suppliers#update'
+    delete '/suppliers/:id' => 'suppliers#destroy'
+
+    get '/images' => 'images#index'
+    post '/images' => 'images#create'
+    get '/images/:id' => 'images#show'
+    patch '/images/:id' => 'images#update'
+    delete '/images/:id' => 'images#destroy'
+    
   end
 end
